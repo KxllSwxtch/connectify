3ae37e0444aa71dbc23d518974a3b350cb71ba45
KxllSwxtch <dimashin9@gmail.com>
2023-03-31 19:12:01 +0900

    initial commit

---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+import Home from "./Home";
+import Auth from './Auth'
+export { Home, Auth }
---
+++ src/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { Home, Auth } from './pages'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{ path: '/auth', element: <Auth /> },
+])
